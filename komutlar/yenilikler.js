1ba3d58c7b36e3709d967dd5c678622b1b6cef0d
denizozenc <39837176+denizozenc@users.noreply.github.com>
2018-06-01 20:13:50 +0300

    Update yenilikler.js

--- komutlar/yenilikler.js
+++ komutlar/yenilikler.js
@@ -5,7 +5,7 @@ exports.run = (client, message, args) => {
   .setColor(0x00AE86)
   .setTimestamp()
   .setAuthor(message.author.username, message.author.avatarURL)
-  .setDescription("**\np!şelale = Şelale Gifi Atar \np!wtf = Yorumsuz \np!havadurumu = İstediğiniz Şehrin Hava Durumunu Atar \np!kullanıcı-ara = Girdiğiniz İsimle Kullanıcı Arar \np!hedef = Hedefimizi Gösterir \nGeldi!**");
+  .setDescription("**\nVDS EKLENMİŞTİR**");
   message.channel.sendEmbed(yenilikler)
 };
 
